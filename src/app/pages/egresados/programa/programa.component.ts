bbc94b8d228e537df7da01779cf2820662a79aae
luisa <luisita0518@unicauca.edu.co>
2019-11-19 01:59:35 -0500

    verCompletrarReg

--- src/app/pages/egresados/programa/programa.component.ts
+++ src/app/pages/egresados/programa/programa.component.ts
@@ -3,6 +3,8 @@ import { Validators, FormControl } from '@angular/forms';
 import { FacultadInterface } from 'src/app/shared/modelos/facultadInterface';
 import { CatalogosService } from 'src/app/shared/servicios/common/catalogos.service';
 import { ProgramaInterface } from 'src/app/shared/modelos/programaInteface';
+import { NivelesEstudioInterface } from 'src/app/shared/modelos/nivelesEstudioInterface';
+import { SedeInterface } from 'src/app/shared/modelos/sedeInterface';
 
 @Component({
   selector: 'app-programa',
@@ -12,22 +14,34 @@ import { ProgramaInterface } from 'src/app/shared/modelos/programaInteface';
 export class ProgramaComponent implements OnInit {
 
   nivelAcademico = new FormControl('', [Validators.required]);
+  sede = new FormControl('', [Validators.required]);
   facultad = new FormControl('', [Validators.required]);
   programa = new FormControl('', [Validators.required]);
 
-  facultades : FacultadInterface[];
-  programas : ProgramaInterface[];
-
+  sedes: SedeInterface[];
+  facultades: FacultadInterface[];
+  programas: ProgramaInterface[];
+  nivelAcade: NivelesEstudioInterface[];
+  
   constructor(private catalogoService:CatalogosService) { }
 
   ngOnInit() {
   }
 
-  obtenerFacultad(){
-    //this.catalogoService.getFacultad().subscribe(data => this.facultades=data);
+  obtenerNivelEstudio(){
+    this.catalogoService.getNivelEducativo().subscribe(data => this.nivelAcade = data);
   }
+
+  obtenerSedes(){
+    this.catalogoService.getSede().subscribe(data => this.sedes = data);
+  }
+
+  obtenerFacultad(){
+    this.catalogoService.getFacultad(this.sede.value).subscribe(data => this.facultades = data);
+  }
+
   obtenerPrograma(){
-    //this.catalogoService.getPrograma(this.facultad.value).subscribe(data => this.programas=data);
+    this.catalogoService.getPrograma(this.sede.value,this.facultad.value,this.nivelAcademico.value).subscribe(data => this.programas = data);
   }
 
 }
