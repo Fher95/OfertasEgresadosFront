a4c9cd8657dc3c7f954114abb308c2c4f91e16f1
Fernando <luisfgomezg@unicauca.edu.co>
2019-12-12 11:46:20 -0500

    correcciones en listar empresas

--- src/app/pages/administrador/info-solicitud-empresa/info-solicitud-empresa.component.ts
+++ src/app/pages/administrador/info-solicitud-empresa/info-solicitud-empresa.component.ts
@@ -1,24 +1,25 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Solicitud } from '../listar-solicitudes-empresa/Solicitud';
 import { ListarSolicitudesEmpresaComponent, DialogData } from '../listar-solicitudes-empresa/listar-solicitudes-empresa.component';
 import { ListarSolicitudesService } from '../listar-solicitudes-empresa/listar-solicitudes.service';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-info-solicitud-empresa',
   templateUrl: './info-solicitud-empresa.component.html',
   styleUrls: ['./info-solicitud-empresa.component.css']
 })
-export class InfoSolicitudEmpresaComponent implements OnInit  {
+export class InfoSolicitudEmpresaComponent implements OnInit {
 
-  seleccionNumOfertas: number = 0 ;
+  seleccionNumOfertas: number = 0;
   seleccionValida = false;
+  limitePublicaciones = 50;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData,
-              private servicioLista: ListarSolicitudesService,
-              private _snackBar: MatSnackBar) {}
-  
+    private servicioLista: ListarSolicitudesService,
+    private _snackBar: MatSnackBar) { }
+
   ngOnInit() {
     console.log('Abierto dialog de información de empresa');
   }
@@ -34,8 +35,10 @@ export class InfoSolicitudEmpresaComponent implements OnInit  {
   }
 
   activacionValida(): void {
-    if (this.seleccionNumOfertas > 0){
-      this.seleccionValida = true;
+    if (this.seleccionNumOfertas > 0) {
+      if (this.seleccionNumOfertas <= this.limitePublicaciones) {
+        this.seleccionValida = true;
+      } else { this.seleccionValida = false; }
     } else {
       this.seleccionValida = false;
     }
@@ -46,8 +49,8 @@ export class InfoSolicitudEmpresaComponent implements OnInit  {
     this.seleccionValida = false;
   }
 
-  activarEmpresa(parSolicitud: Solicitud): void {    
-    if (parSolicitud != null){
+  activarEmpresa(parSolicitud: Solicitud): void {
+    if (parSolicitud != null) {
       this.servicioLista.activarSolicitud(parSolicitud.id_aut_empresa, this.seleccionNumOfertas)
         .subscribe(result => {
           this.openSnackBar('Empresa activada');
@@ -59,7 +62,7 @@ export class InfoSolicitudEmpresaComponent implements OnInit  {
   }
 
   desactivarEmpresa(parSolicitud: Solicitud): void {
-    if (parSolicitud != null){
+    if (parSolicitud != null) {
       this.servicioLista.desactivarSolicitud(parSolicitud.id_aut_empresa)
         .subscribe(result => {
           this.openSnackBar('Empresa desactivada');
