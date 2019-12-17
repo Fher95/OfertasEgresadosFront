442600147529960cc301dd33473b378d29f9c241
bravodan23 <bdaniel@unicauca.edu.co>
2019-12-17 10:18:54 -0600

    redireccion general de home a preregistro egresados eliminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
    constructor(private router: Router) {
-    this.router.navigateByUrl('/egresados/pre-registro');
+    
   } 
 
 }
