ce7df8ba3d87d68fe7e4002ee42416b43e0e476e
bravodan23 <bdaniel@unicauca.edu.co>
2019-12-17 20:16:25 -0500

    registro empresa finalizado

--- src/app/layout/navbar-home-ofertas/navbar-home-ofertas.component.ts
+++ src/app/layout/navbar-home-ofertas/navbar-home-ofertas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { authInterface } from 'src/app/shared/modelos/authInterface';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/servicios/auth/auth.service';
+import { AuthServicesService } from 'src/app/shared/servicios/authServices/auth-services.service';
 
 @Component({
   selector: 'app-navbar-home-ofertas',
@@ -15,31 +16,21 @@ export class NavbarHomeOfertasComponent implements OnInit {
     email: "",
     password: ""
   }
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router, private authService: AuthServicesService) { }
 
   ngOnInit() {
+    console.log(this.auth.userRol);
+    console.log(this.authService.getCurrentUser());
   }
 
-
-  onLogin() {
-    this.router.navigate(['empresa/1/datosEmpresa']);
-    /*console.log(this.user.email)
-    console.log(this.user.password)
-
-    return this.authService.loginUser(this.user.email, this.user.password)
-    .subscribe(data => {
-      console.log(data)
-      this.authService.setUser(data.user)
-      this.authService.setToken(data)
-      this.router.navigate(['/datosEmpresa'])
-    }),
-    error => console.log(error)*/
-  }
   onRegistroEmpresa() {
     this.router.navigate(['registroEmpresa']);
   }
   onEgresados() {
     this.router.navigate(['/egresados']);
+  }
+  onOfertas() {
+    this.router.navigate(['empresa/1/datosEmpresa'])
   }
 
   get isLogin() {
@@ -48,6 +39,10 @@ export class NavbarHomeOfertasComponent implements OnInit {
 
   get isAdmin() {
     return this.auth.userRol ? this.auth.userRol.toUpperCase() === 'ADMIN' : false;
+  }
+
+  get isEmpresa() {
+    return this.auth.userRol ? this.auth.userRol.toUpperCase() === 'EMPRESA' : false;
   }
 
   logout() {
