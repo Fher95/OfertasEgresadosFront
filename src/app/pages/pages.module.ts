bd6fed7c90e49c854bbcd6204c65d3ca3782e4c2
luisa <luisita0518@unicauca.edu.co>
2020-03-10 00:36:47 -0500

    conflictos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,10 +5,7 @@ import { EgresadosModule } from './egresados/egresados.module';
 import { AuthModule } from './auth/auth.module';
 import { DiscapacidadesPipe } from '../shared/pipes/discapacidades.pipe';
 import { EgCapitalizePipe } from '../shared/pipes/eg-capitalize.pipe';
-<<<<<<< HEAD
 import { PipesModule } from '../shared/pipes/pipes.module';
-=======
->>>>>>> 23036a941fa48c9021d19af887705a5998e3e8f9
 
 @NgModule({
   imports: [CommonModule],
