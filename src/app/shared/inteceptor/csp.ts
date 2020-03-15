740ead4ea39ca34213c5471f5835387036f1f5e8
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2020-03-15 02:17:36 -0500

    Solucion rutas

---
+++ src/app/shared/inteceptor/csp.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HeaderInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const headers = req.headers.set(
+      'Content-Security-Policy',
+      "default-src 'self' *.herokuapp.com"
+    );
+    const request = req.clone({ headers });
+    return next.handle(request);
+  }
+}
