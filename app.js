afbcdf7cd432f32b5b139db948e88e23d8a7ac43
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2020-03-15 01:41:23 -0500

    Despliegue en heroku

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const appname = 'ofertas-egresados-front';
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ofertas-egresados'));
+app.use(express.static(__dirname + `/dist/${appname}`));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ofertas-egresados/index.html'));
+  res.sendFile(path.join(__dirname + `/dist/${appname}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
