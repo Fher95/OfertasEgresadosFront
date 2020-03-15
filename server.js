06684b52b7f965ad6eb790d844ea1d66cd6d6ae2
Sebastián Carabali <sebastiancc@unicauca.edu.co>
2020-03-15 01:12:06 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/ofertas-egresados'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ofertas-egresados/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
